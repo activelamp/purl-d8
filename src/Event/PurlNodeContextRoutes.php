1d5621e74b2294ff14a4549a62b5892425145d21
Evan Jenkins <evan@activelamp.com>
2016-08-01 15:25:22 -0700

    removing unused use statements

--- src/Event/PurlNodeContextRoutes.php
+++ src/Event/PurlNodeContextRoutes.php
@@ -2,22 +2,14 @@
 
 namespace Drupal\purl\Event;
 
-use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
-use Drupal\Core\ParamConverter\ParamConverterManagerInterface;
 use Drupal\Core\Render\Element\Url;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Routing\RoutingEvents;
-use Drupal\Core\Routing\RouteBuildEvent;
 use Drupal\purl\MatchedModifiers;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Drupal\Core\Routing\RouteSubscriberBase;
-use Symfony\Component\Routing\RouteCollection;
 use Drupal\Core\Site\Settings;
 use Symfony\Component\HttpFoundation\Response;
 use Drupal\redirect\Exception\RedirectLoopException;
@@ -42,12 +34,11 @@ class PurlNodeContextRoutes implements EventSubscriberInterface {
   protected $matchedModifiers;
 
   /**
-   * Constructs a new PageManagerRoutes.
+   * PurlNodeContextRoutes constructor.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Cache\CacheTagsInvalidatorInterface $cache_tags_invalidator
-   *   The cache tags invalidator.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   * @param RouteMatchInterface        $route_match
+   * @param MatchedModifiers           $matchedModifiers
    */
   public function __construct(EntityTypeManagerInterface $entity_type_manager, RouteMatchInterface $route_match, MatchedModifiers $matchedModifiers) {
     $this->entityStorage = $entity_type_manager->getStorage('node_type');
@@ -56,9 +47,9 @@ class PurlNodeContextRoutes implements EventSubscriberInterface {
   }
 
   /**
-   * Applies parameter converters to route parameters.
+   * Checks if a node's type requires a redirect.
    *
-   * @param \Drupal\Core\Routing\RouteBuildEvent $event
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The event to process.
    */
   public function purlCheckNodeContext(GetResponseEvent $event) {
